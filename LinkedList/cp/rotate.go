a1f753545e0f1a70f95d7aef53545ff389941e79
SKo00o <lwylwy@outlook.com>
2019-06-23 23:05:24 +0800

    complete `Rotate List`

---
+++ LinkedList/cp/rotate.go
@@ -0,0 +1,21 @@
+package cp
+
+// Given a linked list, rotate the list to the right by k places, where k is non-negative.
+func rotateRight(head *ListNode, k int) *ListNode {
+	var len int
+	var p, pre *ListNode
+	for p, pre = head, head; p != nil; pre, p, len = p, p.Next, len+1 {
+	}
+
+	if len == 0 {
+		return head
+	}
+	k = len - (k % len)
+	for pre.Next, p = head, head; k != 0; {
+		pre, p, k = p, p.Next, k-1
+	}
+
+	head = p
+	pre.Next = nil
+	return head
+}
---
+++ LinkedList/cp/rotate_test.go
@@ -0,0 +1,43 @@
+package cp
+
+import (
+	"testing"
+)
+
+func Test_rotateRight(t *testing.T) {
+	tasks := []struct {
+		list   []int
+		val    int
+		expect []int
+	}{
+		{
+			list:   []int{1, 2, 3, 4, 5},
+			val:    2,
+			expect: []int{4, 5, 1, 2, 3},
+		},
+		{
+			list:   []int{0, 1, 2},
+			val:    4,
+			expect: []int{2, 0, 1},
+		},
+		{
+			list:   []int{},
+			val:    0,
+			expect: []int{},
+		},
+	}
+
+	for fIdx, f := range []func(*ListNode, int) *ListNode{rotateRight} {
+		for i, task := range tasks {
+			h1 := makeLinkedList(task.list...)
+			h2 := f(h1, task.val)
+			var got []int
+			for p := h2; p != nil; p = p.Next {
+				got = append(got, p.Val)
+			}
+			if !equal(got, task.expect) {
+				t.Errorf("func #%d, task #%d failed, output: %v, expect: %v", fIdx, i, got, task.expect)
+			}
+		}
+	}
+}
