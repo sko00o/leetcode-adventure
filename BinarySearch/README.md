33830d72530c0f62b44d985ca257e68a554319db
SKo00o <lwylwy@outlook.com>
2019-06-25 00:59:09 +0800

    complete `Binary Search`

---
+++ BinarySearch/README.md
@@ -0,0 +1,5 @@
+# Binary Search
+
+## [Background](https://leetcode.com/explore/learn/card/binary-search/138/background/971/)
+
+* [Binary Search](bs/search.go)
---
+++ BinarySearch/bs/search.go
@@ -0,0 +1,26 @@
+package bs
+
+func search(nums []int, target int) int {
+	maxIdx := len(nums) - 1
+	for l, h := 0, maxIdx; l <= h; {
+		if l >= 0 && l <= maxIdx && nums[l] == target {
+			return l
+		}
+		if h <= maxIdx && h >= 0 && nums[h] == target {
+			return h
+		}
+
+		m := l + ((h - l) >> 1)
+		mid := nums[m]
+		if mid == target {
+			return m
+		} else if mid > target {
+			l = l + 1
+			h = m - 1
+		} else {
+			l = m + 1
+			h = h - 1
+		}
+	}
+	return -1
+}
