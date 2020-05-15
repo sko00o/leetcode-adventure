a626823ef568b8a2801eb6740acfac082ebe2332
sko00o <lwylwy@outlook.com>
2020-05-15 15:51:50 +0800

    Destroy Island in BFS.

--- queue-stack/practice/island.go
+++ queue-stack/practice/island.go
@@ -9,6 +9,14 @@ type Queue struct {
 	queue.Queue
 }
 
+func isInvalid(grid [][]byte, i, j int) bool {
+	return i < 0 ||
+		i >= len(grid) ||
+		j < 0 ||
+		j >= len(grid[0]) ||
+		grid[i][j] == '0'
+}
+
 type pos [2]int
 
 func numIslands(grid [][]byte) int {
@@ -82,17 +90,12 @@ func numIslands1(grid [][]byte) int {
 	return cnt
 }
 
+// DFS
 func distroyIsland(grid [][]byte, i, j int) int {
-	var li, lj int
-	if li = len(grid); li == 0 {
+	if isInvalid(grid, i, j) {
 		return 0
 	}
-	if lj = len(grid[0]); lj == 0 {
-		return 0
-	}
-	if i < 0 || i >= li || j < 0 || j >= lj || grid[i][j] == '0' {
-		return 0
-	}
+
 	grid[i][j] = '0'
 	for _, d := range []struct{ i, j int }{
 		{0, 1},  // right
@@ -101,6 +104,111 @@ func distroyIsland(grid [][]byte, i, j int) int {
 		{-1, 0}, // top
 	} {
 		distroyIsland(grid, i+d.i, j+d.j)
+	}
+	return 1
+}
+
+func numIslands2(grid [][]byte) int {
+	visited := make([][]bool, len(grid))
+	for i := range grid {
+		visited[i] = make([]bool, len(grid[i]))
+	}
+
+	// store the num of islands
+	var cnt int
+	var queue [][2]int
+	li := len(grid)
+	for i := li - 1; i >= 0; i-- {
+		lj := len(grid[i])
+		for j := lj - 1; j >= 0; j-- {
+			curr := [2]int{i, j}
+			if visited[i][j] {
+				continue
+			}
+			visited[i][j] = true
+
+			// this is an island
+			if grid[curr[0]][curr[1]] == '1' {
+				queue = append(queue, curr)
+				cnt++
+
+				// visit an island in BFS
+				for len(queue) != 0 {
+					curr := queue[0]
+					for _, d := range [][2]int{
+						{0, 1},  // right
+						{1, 0},  // down
+						{0, -1}, // left
+						{-1, 0}, // top
+					} {
+						ii, jj := curr[0]+d[0], curr[1]+d[1]
+						if ii < li && jj < lj &&
+							ii >= 0 && jj >= 0 {
+							curr := [2]int{ii, jj}
+							if visited[ii][jj] {
+								continue
+							}
+							visited[ii][jj] = true
+
+							if grid[ii][jj] == '1' {
+								queue = append(queue, curr)
+							}
+						}
+					}
+
+					queue = queue[1:]
+				}
+			}
+		}
+	}
+
+	return cnt
+}
+
+// destroy version
+func numIslands3(grid [][]byte) int {
+	var cnt int
+	li := len(grid)
+	for i := li - 1; i >= 0; i-- {
+		lj := len(grid[i])
+		for j := lj - 1; j >= 0; j-- {
+			cnt += distroyIslandBFS(grid, i, j)
+		}
+	}
+
+	return cnt
+}
+
+func distroyIslandBFS(grid [][]byte, i, j int) int {
+	if isInvalid(grid, i, j) {
+		return 0
+	}
+	if grid[i][j] == '0' {
+		return 0
+	}
+
+	var queue [][2]int
+	grid[i][j] = '0'
+	queue = append(queue, [2]int{i, j})
+
+	// visit an island in BFS
+	for len(queue) != 0 {
+		curr := queue[0]
+		for _, d := range [][2]int{
+			{0, 1},  // right
+			{1, 0},  // down
+			{0, -1}, // left
+			{-1, 0}, // top
+		} {
+			ii, jj := curr[0]+d[0], curr[1]+d[1]
+			if isInvalid(grid, ii, jj) {
+				continue
+			}
+			grid[ii][jj] = '0'
+			queue = append(queue, [2]int{ii, jj})
+		}
+
+		queue = queue[1:]
 	}
 	return 1
 }
--- queue-stack/practice/island_test.go
+++ queue-stack/practice/island_test.go
@@ -51,20 +51,51 @@ func Test_numIslands(t *testing.T) {
 			},
 			want: 4,
 		},
+		{
+			name: "test 2",
+			args: args{
+				grid: [][]byte{
+					[]byte("110000"),
+					[]byte("011110"),
+					[]byte("010010"),
+					[]byte("010010"),
+					[]byte("011110"),
+					[]byte("000000"),
+				},
+			},
+			want: 1,
+		},
+		{
+			name: "empty",
+			want: 0,
+		},
 	}
 
 	for idx, f := range []func(grid [][]byte) int{
 		numIslands,
 		numIslands1,
+		numIslands2,
+		numIslands3,
 	} {
 		t.Run(fmt.Sprint("func#d", idx), func(t *testing.T) {
 			for _, tt := range tests {
 				t.Run(tt.name, func(t *testing.T) {
-					if got := f(tt.args.grid); got != tt.want {
+					if got := f(copyGrid(tt.args.grid)); got != tt.want {
 						t.Errorf("numIslands() = %v, want %v", got, tt.want)
 					}
 				})
 			}
 		})
 	}
+}
+
+func copyGrid(grid [][]byte) (out [][]byte) {
+	out = make([][]byte, len(grid))
+	for i := range grid {
+		out[i] = make([]byte, len(grid[i]))
+		for j := range grid[i] {
+			out[i][j] = grid[i][j]
+		}
+	}
+	return
 }
