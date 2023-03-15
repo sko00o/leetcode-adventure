f6723da95a2ceb2f0094c02b06e44ea4ce1b535b
sko00o <lwylwy@outlook.com>
2023-03-16 00:05:06 +0800

    N-ary tree preorder traversal

--- nary-tree/traversal/preorder.go
+++ nary-tree/traversal/preorder.go
@@ -1,6 +1,36 @@
 package traversal
 
+// recursively
+// time complexity: O(N), N is the number of nodes in the given tree
+// space complexity: O(H), H is the height of the given tree
 func preorder(root *Node) []int {
-    // TODO
-	return nil
+	if root == nil {
+		return nil
+	}
+	var res = []int{root.Val}
+	for _, child := range root.Children {
+		res = append(res, preorder(child)...)
+	}
+	return res
+}
+
+// iteratively using stack
+// time complexity: O(N), N is the number of nodes in the given tree
+// space complexity: O(H), H is the height of the given tree
+func preorder1(root *Node) (out []int) {
+	var stack []*Node
+	for curr := root; curr != nil || len(stack) != 0; {
+		if curr != nil {
+			out = append(out, curr.Val)
+			for i := len(curr.Children) - 1; i >= 0; i-- {
+				stack = append(stack, curr.Children[i]) // push
+			}
+			curr = nil
+		}
+		if len(stack) != 0 {
+			curr = stack[len(stack)-1]   // top
+			stack = stack[:len(stack)-1] // pop
+		}
+	}
+	return out
 }
--- nary-tree/traversal/preorder_test.go
+++ nary-tree/traversal/preorder_test.go
@@ -1,7 +1,91 @@
 package traversal
 
-import "testing"
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
 
 func Test_preorder(t *testing.T) {
+	type args struct {
+		root *Node
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{
+			name: "example 1",
+			args: args{
+				root: &Node{
+					Val: 1,
+					Children: []*Node{
+						{
+							Val: 3,
+							Children: []*Node{
+								{Val: 5},
+								{Val: 6},
+							},
+						},
+						{Val: 2},
+						{Val: 4},
+					},
+				},
+			},
+			want: []int{1, 3, 5, 6, 2, 4},
+		},
+		{
+			name: "example 2",
+			args: args{
+				root: &Node{
+					Val: 1,
+					Children: []*Node{
+						{Val: 2},
+						{
+							Val: 3,
+							Children: []*Node{
+								{Val: 6},
+								{Val: 7, Children: []*Node{
+									{Val: 11, Children: []*Node{
+										{Val: 14},
+									}},
+								}},
+							},
+						},
+						{Val: 4, Children: []*Node{
+							{Val: 8, Children: []*Node{
+								{Val: 12},
+							}},
+						}},
+						{Val: 5, Children: []*Node{
+							{Val: 9, Children: []*Node{
+								{Val: 13},
+							}},
+							{Val: 10},
+						}},
+					},
+				},
+			},
+			want: []int{1, 2, 3, 6, 7, 11, 14, 4, 8, 12, 5, 9, 13, 10},
+		},
+		{
+			name: "empty",
+		},
+	}
 
+	for idx, f := range []func(*Node) []int{
+		preorder,
+		preorder1,
+	} {
+		t.Run(fmt.Sprintf("func#%d", idx), func(t *testing.T) {
+			for _, tt := range tests {
+				t.Run(tt.name, func(t *testing.T) {
+					if got := f(tt.args.root); !reflect.DeepEqual(got, tt.want) {
+						t.Errorf("preorder() = %v, want %v", got, tt.want)
+					}
+				})
+			}
+		})
+	}
 }
