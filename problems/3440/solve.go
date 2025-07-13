8a57e3c6b0318ef4c639c808d6b204a8c34788a5
sko00o <lwylwy@outlook.com>
2025-07-13 11:23:13 +0800

    accepted 3440

--- problems/3440/solve.go
+++ problems/3440/solve.go
@@ -1,6 +1,5 @@
 package problems
 
-// Still TLE
 func maxFreeTime(eventTime int, startTime []int, endTime []int) int {
 	gapLen := func(n int) int {
 		if n == 0 {
@@ -11,23 +10,49 @@ func maxFreeTime(eventTime int, startTime []int, endTime []int) int {
 		}
 		return startTime[n] - endTime[n-1]
 	}
-	gapList := make([]gap, 0, len(startTime)+1)
+	gapList := make([]int, 0, len(startTime)+1)
 	{
 		gapStart := 0
 		for i := 0; i < len(startTime); i++ {
 			gapVal := startTime[i] - gapStart
-			gapList = append(gapList, gap{idx: i, val: gapVal})
+			gapList = append(gapList, gapVal)
 			gapStart = endTime[i]
 		}
-		gapList = append(gapList, gap{idx: len(startTime), val: eventTime - gapStart})
+		gapList = append(gapList, eventTime-gapStart)
 	}
-	quickSort(gapList, 0, len(gapList)-1)
+
+	n := len(gapList)
+	prefixMax := make([]int, n)
+	suffixMax := make([]int, n)
+	prefixMax[0] = gapList[0]
+	for i := 1; i < n; i++ {
+		if gapList[i] > prefixMax[i-1] {
+			prefixMax[i] = gapList[i]
+		} else {
+			prefixMax[i] = prefixMax[i-1]
+		}
+	}
+	suffixMax[n-1] = gapList[n-1]
+	for i := n - 2; i >= 0; i-- {
+		if gapList[i] > suffixMax[i+1] {
+			suffixMax[i] = gapList[i]
+		} else {
+			suffixMax[i] = suffixMax[i+1]
+		}
+	}
 
 	maxFreeTime := 0
 	for i := range startTime {
 		evLen := endTime[i] - startTime[i]
 		freeTime := gapLen(i) + gapLen(i+1)
-		if hasBiggerGap(gapList, evLen, i) {
+		maxOtherGap := 0
+		if i-1 >= 0 {
+			maxOtherGap = prefixMax[i-1]
+		}
+		if i+2 < n && suffixMax[i+2] > maxOtherGap {
+			maxOtherGap = suffixMax[i+2]
+		}
+		if maxOtherGap >= evLen {
 			freeTime += evLen
 		}
 		if maxFreeTime < freeTime {
@@ -35,45 +60,4 @@ func maxFreeTime(eventTime int, startTime []int, endTime []int) int {
 		}
 	}
 	return maxFreeTime
-}
-
-type gap struct {
-	idx int
-	val int
-}
-
-// first index of arr val that is bigger than val, notI is excluded
-func hasBiggerGap(arr []gap, val int, notI int) bool {
-	hi := len(arr) - 1
-	if arr[hi].val < val {
-		return false
-	}
-	for hi >= 0 && (arr[hi].idx == notI || arr[hi].idx == notI+1) {
-		hi--
-	}
-	if hi < 0 {
-		return false
-	}
-	return arr[hi].val >= val
-}
-
-func quickSort(arr []gap, lo, hi int) {
-	if lo >= hi || lo < 0 {
-		return
-	}
-	p := partition(arr, lo, hi)
-	quickSort(arr, lo, p-1)
-	quickSort(arr, p+1, hi)
-}
-func partition(arr []gap, lo, hi int) int {
-	pivot := arr[hi]
-	i := lo
-	for j := lo; j < hi; j++ {
-		if arr[j].val < pivot.val {
-			arr[i], arr[j] = arr[j], arr[i]
-			i++
-		}
-	}
-	arr[i], arr[hi] = arr[hi], arr[i]
-	return i
 }
